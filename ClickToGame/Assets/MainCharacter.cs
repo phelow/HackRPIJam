e543f45a5caf331affbeeeb16e90c1913c8584e7
phelow <wpheloung@gmail.com>
2016-11-12 13:08:30 -0500

    first commit

---
+++ ClickToGame/Assets/MainCharacter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_isLeft;
+    // Use this for initialization
+
+    [SerializeField]
+    private Transform m_parentCharacterTransform;
+
+    void Start()
+    {
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        m_isLeft = !m_isLeft;
+
+        m_parentCharacterTransform.Rotate(new Vector3(0, 0, 180));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            ChangeDirection();
+            Debug.Log("Changing Directions");
+        }
+    }
+}
